9108d86cdb55c0171d84822a1621c94e49f7ea85
North Africa Business Intelligence <mouafiq@gmail.com>
2019-08-07 11:52:37 +0100

    Create __openerp__.py

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Extension de gestion de stock',
+    'version': '1.1',
+    'category': 'Stock',
+    'description': """
+    Extension de gestion de stock
+    """,
+    'author': 'N.A.B.I sarl',
+    'depends': ['stock'],
+    'data': [    ],
+    'installable': True,
+    'auto_install': False,
+}
+
